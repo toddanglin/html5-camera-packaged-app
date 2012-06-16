8c0941e99601757098884e729495b905c192109f
Todd Anglin <anglin@telerik.com>
2012-06-15 23:24:01 -0500

    Tweaked the app window size and position on load to better display entire app

--- background.js
+++ background.js
@@ -4,7 +4,16 @@ chrome.experimental.app.onLaunched.addListener(function() {
   	console.log(win)
   }
 
-  var win = chrome.appWindow.create('chrome/main.html', { width: 900, height: 700 }, onWindowLoaded);
+  //TODO: minW/H are not observed yet
+  var win = chrome.appWindow.create('chrome/main.html', { 
+  	width: 1024, 
+  	height: 870,
+  	minWidth:900,
+  	minHeight:800,
+  	left:500,
+  	top:500
+  	}, 
+  	onWindowLoaded);
 
 });
 
